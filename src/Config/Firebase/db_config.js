c9e163c68933c4d0a3b015657745c7c41db1fff0
Jessamyn27 <jessamyn.mctwigan@gmail.com>
2018-08-28 14:49:38 -0500

    flashcards and to-do's up and running with firebase

---
+++ src/Config/Firebase/db_config.js
@@ -0,0 +1,16 @@
+export const DB_CONFIG = {
+//   <script src="https://www.gstatic.com/firebasejs/5.4.1/firebase.js"></script>
+// <script>
+//   // Initialize Firebase
+   // config = {
+    apiKey: "AIzaSyBDJGz3_DoOolsc2DSJgeYl2lTwp0O8e1Q",
+    authDomain: "react-flashcards-80665.firebaseapp.com",
+    databaseURL: "https://react-flashcards-80665.firebaseio.com",
+    projectId: "react-flashcards-80665",
+    storageBucket: "react-flashcards-80665.appspot.com",
+    messagingSenderId: "433015618711"
+  }
+
+  // firebase.initializeApp(config);
+// </script>
+// }
